ca60de2655495ee83647d21805c6a11cc269b6d9
jimmy <beibe218@163.com>
2018-09-16 15:26:28 +0800

    first commit

--- app/src/main/java/com/jimmy/socketserver/MainActivity.java
+++ app/src/main/java/com/jimmy/socketserver/MainActivity.java
@@ -1,13 +1,8 @@
 package com.jimmy.socketserver;
 
 import android.content.Intent;
-import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import java.net.Inet4Address;
-import java.net.UnknownHostException;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
