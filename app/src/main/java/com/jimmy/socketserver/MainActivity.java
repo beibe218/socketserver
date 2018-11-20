11f8b8e456066ac22bf823dc8cb7bc0604be19b0
Gijimmy <Gibeibe218@163.com>
2018-11-20 19:34:09 +0800

    新增登录、发送消息功能

--- app/src/main/java/com/jimmy/socketserver/MainActivity.java
+++ app/src/main/java/com/jimmy/socketserver/MainActivity.java
@@ -1,16 +1,109 @@
 package com.jimmy.socketserver;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    SocketServer.SocketServerListener socketServerListener = new SocketServer.SocketServerListener() {
+        @Override
+        public void onNotifyClient(List<Socket> clients) {
+            listAdapter.replaceDatas(clients);
+        }
+
+        @Override
+        public void onNotifyNewMessage(String newMessage) {
+            Toast.makeText(MainActivity.this, newMessage, Toast.LENGTH_LONG).show();
+        }
+    };
+    private ListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.client_list);
+        listAdapter = new ListAdapter();
+        listView.setAdapter(listAdapter);
+
+        ServiceConnection conn = new ServiceConnection() {
+
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                SocketServer socketServer = ((SocketServer.MyBinder) service).getSocketServer();
+                socketServer.setSocketServerListener(socketServerListener);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        };
+
         Intent intent = new Intent(this, SocketServer.class);
-        startService(intent);
+        bindService(intent, conn, BIND_AUTO_CREATE);
+    }
+
+    private class ListAdapter extends BaseAdapter {
+
+        private List<Socket> datas = new ArrayList<>();
+
+        @Override
+        public int getCount() {
+            return datas == null ? 0 : datas.size();
+        }
+
+        @Override
+        public Socket getItem(int position) {
+            return datas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.client_item_layout, parent, false);
+                holder.text1 = convertView.findViewById(R.id.text1);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Socket socket = getItem(position);
+            holder.text1.setText(socket.getInetAddress().getHostAddress());
+
+            return convertView;
+        }
+
+        public void replaceDatas(List<Socket> datas) {
+            this.datas = datas;
+            notifyDataSetChanged();
+        }
+
+        private class ViewHolder {
+            TextView text1;
+        }
     }
 }
--- app/src/main/java/com/jimmy/socketserver/SocketServer.java
+++ app/src/main/java/com/jimmy/socketserver/SocketServer.java
@@ -1,42 +1,99 @@
 package com.jimmy.socketserver;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static com.jimmy.socketserver.SocketServer.Status.JOIN;
+import static com.jimmy.socketserver.SocketServer.Status.NEW_MESSAGE;
+
 public class SocketServer extends Service {
     private static final String TAG = "SocketServer";
+    //Channel ID 必须保证唯一
+    private static final String CHANNEL_ID = "com.jimmy.socket.notification.channel";
 
     Map<String, Socket> clients;
     private boolean isRunningServer = false;
     private ExecutorService threadPool;
+    private MyHandler myHandler = new MyHandler(this);
+    private SocketServerListener listener;
+
+    public class MyBinder extends Binder {
+        public SocketServer getSocketServer() {
+            return SocketServer.this;
+        }
+    }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new MyBinder();
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setForegroundService();
+        }
+
         this.clients = new HashMap<>();
         this.isRunningServer = true;
         this.threadPool = Executors.newFixedThreadPool(30);
 
         SocketServerThread serverThread = new SocketServerThread();
         threadPool.execute(serverThread);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setForegroundService() {
+        //设置通知的重要程度
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        //构建通知渠道
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "SocketServer", importance);
+        channel.setDescription("SocketServer");
+
+        //在创建的通知渠道上发送通知
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setSmallIcon(R.mipmap.ic_launcher) //设置通知图标
+                .setContentTitle("SocketServer")//设置通知标题
+                .setContentText("New message")//设置通知内容
+                .setAutoCancel(true) //用户触摸时，自动关闭
+                .setOngoing(true);//设置处于运行状态
+
+        //向系统注册通知渠道，注册后不能改变重要性以及其他通知行为
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        //将服务置于启动状态 NOTIFICATION_ID指的是创建的通知的ID
+        startForeground(7100, builder.build());
     }
 
     private class SocketServerThread extends Thread {
@@ -46,11 +103,9 @@ public class SocketServer extends Service {
         @Override
         public void run() {
             try {
-                serverSocket = new ServerSocket(10086);
+                serverSocket = new ServerSocket(7100);
                 while (isRunningServer) {
                     Socket client = serverSocket.accept();
-                    String hostAddress = client.getInetAddress().getHostAddress();
-                    clients.put(hostAddress, client);
                     SocketClientReadingThread readingThread = new SocketClientReadingThread(client);
                     threadPool.execute(readingThread);
                 }
@@ -78,10 +133,31 @@ public class SocketServer extends Service {
         public void run() {
             try {
                 while (isRunningServer) {
-                    Log.d(TAG, socket.getInetAddress().getHostAddress() + "=" + dis.readUTF());
+                    Message msg = Message.obtain();
+                    String s = dis.readUTF();
+                    String[] strings = s.split("&");
+                    if ("join".equals(strings[0])) {
+                        msg.what = JOIN;
+                        clients.put(strings[1], socket);
+                    } else if ("message".equals(strings[0])) {
+                        msg.what = NEW_MESSAGE;
+                        msg.obj = "from " + strings[1] + "，msg " + strings[2];
+                        sendMessageToClient(strings[1], strings[2]);
+                    }
+                    myHandler.sendMessage(msg);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendMessageToClient(String userName, String message) throws IOException {
+        for (Map.Entry<String, Socket> e : clients.entrySet()) {
+            if (!userName.equals(e.getKey())) {
+                Socket client = e.getValue();
+                DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+                dos.writeUTF(message);
             }
         }
     }
@@ -90,6 +166,62 @@ public class SocketServer extends Service {
     public void onDestroy() {
         super.onDestroy();
         isRunningServer = false;
+        stopForeground(true);
+    }
+
+    public void setSocketServerListener(SocketServerListener listener) {
+        this.listener = listener;
+    }
+
+    public interface SocketServerListener {
+        void onNotifyClient(List<Socket> clients);
+
+        void onNotifyNewMessage(String newMessage);
+    }
+
+    private static class MyHandler extends Handler {
+
+        private WeakReference<SocketServer> weakReference;
+
+        public MyHandler(SocketServer socketServer) {
+            this.weakReference = new WeakReference<>(socketServer);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            SocketServer socketServer = weakReference.get();
+            if (socketServer != null) {
+                if (JOIN == msg.what) {
+                    socketServer.notifyClients();
+                } else if (NEW_MESSAGE == msg.what) {
+                    socketServer.notifyNewMessage((String) msg.obj);
+                }
+            }
+        }
+    }
+
+    public void notifyClients() {
+        if (listener == null)
+            return;
+
+        List<Socket> clientList = new ArrayList<>();
+        for (Map.Entry<String, Socket> e : clients.entrySet()) {
+            clientList.add(e.getValue());
+        }
+
+        listener.onNotifyClient(clientList);
+    }
+
+    public void notifyNewMessage(String newMessage) {
+        if (listener == null)
+            return;
+
+        listener.onNotifyNewMessage(newMessage);
+    }
+
+    public static final class Status {
+        public static final int JOIN = 0;
+        public static final int NEW_MESSAGE = 1;
     }
 
 }
